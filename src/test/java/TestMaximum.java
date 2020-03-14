134ddbcb53d030bccea753d862dd6813e3b184de
vampire16 <siddheshsalvi525@gmai.com>
2020-03-14 18:00:52 +0530

    ADDED : New test methods to check maximum number

--- src/test/java/TestMaximum.java
+++ src/test/java/TestMaximum.java
@@ -1,2 +1,27 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestMaximum {
+    Maximum max;
+    @Test
+    public void givenIntegers_WhenMaxAtPositionOne_ShouldReturnMaximum() {
+        max = new Maximum();
+        Integer maximum = Maximum.findMaxInt(5,4,3);
+        Assert.assertEquals(5,(int)maximum);
+    }
+
+    @Test
+    public void givenIntegers_WhenMaxAtPositionTwo_ShouldReturnMaximum() {
+        max = new Maximum();
+        Integer maximum = Maximum.findMaxInt(4, 5, 3);
+        Assert.assertEquals(5, (int) maximum);
+    }
+
+    @Test
+    public void givenIntegers_WhenMaxAtPositionThree_ShouldReturnMaximum() {
+        max = new Maximum();
+        Integer maximum = Maximum.findMaxInt(3, 4, 5);
+        Assert.assertEquals(5, (int) maximum);
+    }
+
 }
