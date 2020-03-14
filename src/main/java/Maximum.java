39cea8aec86a2f9182a90fd334579659cb51c9e4
vampire16 <siddheshsalvi525@gmai.com>
2020-03-14 18:00:04 +0530

    ADDED : Method of findMaxInt to find maximum number from three integers

--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -2,4 +2,14 @@ public class Maximum {
     public static void main(String[] args){
         System.out.println("Welcome");
     }
+
+    public static Integer findMaxInt(Integer number1, Integer number2, Integer number3) {
+        Integer maximum = number1;
+
+        if (number2.compareTo(maximum)>0)
+            maximum = number2;
+        if (number3.compareTo(maximum)>0)
+            maximum = number3;
+        return maximum;
+    }
 }
