40d50dd66eaa5ea5288503bd0a8a17d8bf836371
vampire16 <siddheshsalvi525@gmai.com>
2020-03-16 09:28:53 +0530

    REFACTORED : Replaced name of all methods into name of generic method

--- src/test/java/TestMaximum.java
+++ src/test/java/TestMaximum.java
@@ -4,57 +4,56 @@ import org.junit.Test;
 public class TestMaximum {
     @Test
     public void givenIntegers_WhenMaxAtPositionOne_ShouldReturnMaximum() {
-        Integer maximum = Maximum.findMaxInt(5,4,3);
+        Integer maximum = Maximum.findMax(5,4,3);
         Assert.assertEquals((Integer)5,maximum);
     }
 
     @Test
     public void givenIntegers_WhenMaxAtPositionTwo_ShouldReturnMaximum() {
-        Integer maximum = Maximum.findMaxInt(4, 5, 3);
+        Integer maximum = Maximum.findMax(4, 5, 3);
         Assert.assertEquals((Integer)5, maximum);
     }
 
     @Test
     public void givenIntegers_WhenMaxAtPositionThree_ShouldReturnMaximum() {
-        Integer maximum = Maximum.findMaxInt(3, 4, 5);
+        Integer maximum = Maximum.findMax(3, 4, 5);
         Assert.assertEquals((Integer)5,maximum);
     }
 
     @Test
     public void givenFloats_WhenMaxAtPositionOne_ShouldReturnMaximum() {
-        Float maximum = Maximum.findMaxFloat(5.2f,4.1f,3.4f);
+        Float maximum = Maximum.findMax(5.2f,4.1f,3.4f);
         Assert.assertEquals((Float)5.2f,maximum);
     }
 
     @Test
     public void givenFloats_WhenMaxAtPositionTwo_ShouldReturnMaximum() {
-        Float maximum = Maximum.findMaxFloat(4.1f,5.2f,3.4f);
+        Float maximum = Maximum.findMax(4.1f,5.2f,3.4f);
         Assert.assertEquals((Float)5.2f,maximum);
     }
 
     @Test
     public void givenFloats_WhenMaxAtPositionThree_ShouldReturnMaximum() {
-        Float maximum = Maximum.findMaxFloat(3.4f,4.1f,5.2f);
+        Float maximum = Maximum.findMax(3.4f,4.1f,5.2f);
         Assert.assertEquals((Float)5.2f,maximum);
     }
 
     @Test
     public void givenStrings_WhenMaxAtPositionOne_ShouldReturnMaximum() {
-        String maximum = Maximum.findMaxString("C","B","A");
+        String maximum = Maximum.findMax("C","B","A");
         Assert.assertEquals((String)"C",maximum);
     }
 
     @Test
     public void givenStrings_WhenMaxAtPositionTwo_ShouldReturnMaximum() {
-        String maximum = Maximum.findMaxString("A","C","B");
+        String maximum = Maximum.findMax("A","C","B");
         Assert.assertEquals((String)"C",maximum);
     }
 
     @Test
     public void givenStrings_WhenMaxAtPositionThree_ShouldReturnMaximum() {
-        String maximum = Maximum.findMaxString("A","B","C");
+        String maximum = Maximum.findMax("A","B","C");
         Assert.assertEquals((String)"C",maximum);
     }
-
 
 }
